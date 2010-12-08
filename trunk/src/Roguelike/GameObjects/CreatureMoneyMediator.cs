29de3152f08c2c87dcf96088bd817c65c73a7941
lukasz.wiatrak@gmail.com <lukasz.wiatrak@gmail.com@8b96264e-37d3-a6e0-c8de-c9e3943d66e0>
2010-12-08 21:53:54 +0000

    Zaimplementowana częściowo możliwość chodzenia po mapie. Póki co jednak nie działa zbyt dobrze

---
+++ trunk/src/Roguelike/GameObjects/CreatureMoneyMediator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Roguelike.GameObjects
+{
+	public class CreatureMoneyMediator : IObjectMediator
+	{
+		private Creature creature;
+		private Money money;
+		public CreatureMoneyMediator(Creature creature, Money money)
+		{
+			this.creature = creature;
+			this.money = money;
+		}
+
+		public void mediate()
+		{
+			this.creature.Money += money.Worth;
+		}
+	}
+}
